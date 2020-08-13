95d022d7d773e6fc39b72045b7c62b7843d0c8fe
Aaron Shew <ashew@astate.edu>
2020-08-13 13:34:42 -0500

    beginning map code

---
+++ delta_maps.R
@@ -0,0 +1,35 @@
+### Maps for Lower Mississippi River Alluvial Plain
+
+# Contributes to studies of weather, agriculture, and groundwater/surfacewater projects
+
+# Clear Environment and Load Functional Libraries/Dependencies
+rm(list=ls())
+get <- c("RColorBrewer","raster","sf","ggplot2", "GSODR","tidyverse")
+have   <- get %in% rownames(installed.packages())
+if(any(!have)) install.packages(get[!have])
+sapply(get, function(i) require(i, character.only=TRUE))
+options(digits = 6)
+
+
+# Get shapefiles
+us <- 
+ar
+ear
+
+# Get ag data
+
+
+
+# Get water data
+
+
+
+# County Map
+
+
+
+# Watershed Map
+
+
+
+# Raster Map
